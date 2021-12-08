5c0896e249984920c092fbe0daf1bc97826feb15
S-hayeon <hayeona92@gmail.com>
2021-12-08 20:05:26 +0900

    revert to origin

--- manifest/installer.sh
+++ manifest/installer.sh
@@ -9,12 +9,14 @@ function prepare_online(){
   echo  "========================  Preparing for GitLab =========================="
   echo  "========================================================================="
 
+  curl -s "https://raw.githubusercontent.com/tmax-cloud/catalog/$templateVersion/gitlab/template.yaml" -o "$install_dir/yaml/template.yaml"
+
   sudo docker pull "gitlab/gitlab-ce:13.6.4-ce.0"
-  sudo docker pull "bitnami/kubectl:1.20.10"
+  sudo docker pull "bitnami/kubectl:latest"
   sudo docker tag "gitlab/gitlab-ce:13.6.4-ce.0" "gitlab:13.6.4-ce.0"
-  sudo docker tag "bitnami/kubectl:1.20.10" "kubectl:1.20.10"
+  sudo docker tag "bitnami/kubectl:latest" "kubectl:latest"
   sudo docker save "gitlab:13.6.4-ce.0" > "$install_dir/tar/gitlab_13.6.4-ce.0.tar"
-  sudo docker save "kubectl:1.20.10" > "$install_dir/tar/kubectl_1_20_10.tar"
+  sudo docker save "kubectl:latest" > "$install_dir/tar/kubectl_latest.tar"
 }
 
 function prepare_offline(){
@@ -23,11 +25,11 @@ function prepare_offline(){
   echo  "========================================================================="
 
   sudo docker load < "$install_dir/tar/gitlab_13.6.4-ce.0.tar"
-  sudo docker load < "$install_dir/tar/kubectl_1_20_10.tar"
+  sudo docker load < "$install_dir/tar/kubectl_latest.tar"
   sudo docker tag "gitlab:13.6.4-ce.0" "$imageRegistry/gitlab:13.6.4-ce.0"
-  sudo docker tag "kubectl:1.20.10" "$imageRegistry/kubectl:1.20.10"
+  sudo docker tag "kubectl:latest" "$imageRegistry/kubectl:latest"
   sudo docker push "$imageRegistry/gitlab:13.6.4-ce.0"
-  sudo docker push "$imageRegistry/kubectl:1.20.10"
+  sudo docker push "$imageRegistry/kubectl:latest"
 }
 
 function install(){
@@ -37,8 +39,7 @@ function install(){
 
   # Apply ClusterTemplate
   if [[ "$imageRegistry" == "" ]]; then
-    cp "$install_dir/yaml/template.yaml" "$install_dir/yaml/template_modified.yaml"
-    kubectl apply -f "$install_dir/yaml/template_modified.yaml" "$kubectl_opt"
+    kubectl apply -f "https://raw.githubusercontent.com/tmax-cloud/catalog/$templateVersion/gitlab/template.yaml" "$kubectl_opt"
   else
     cp "$install_dir/yaml/template.yaml" "$install_dir/yaml/template_modified.yaml"
     sed -i -E "s/gitlab\/gitlab-ce\:13.6.4-ce.0/$imageRegistry\/gitlab\:13.6.4-ce.0/g" "$install_dir/yaml/template_modified.yaml"
